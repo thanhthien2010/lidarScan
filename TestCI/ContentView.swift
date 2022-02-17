9b19168816085f2d106eff86fdcf315aed661e7d
thanhthien2010 <thanhthien2010@gmail.com>
2022-02-17 10:28:50 +0700

    - add sources

---
+++ TestCI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestCI
+//
+//  Created by Thanh Thien on 16/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestCI/TestCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCIApp.swift
+//  TestCI
+//
+//  Created by Thanh Thien on 16/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
